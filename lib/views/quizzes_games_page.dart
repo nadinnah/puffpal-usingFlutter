f55816373ad8c3747e1289c3d623d3c460e69e09
nadinnah <nadinahmedmohamed@gmail.com>
2025-11-13 15:40:35 +0200

    add media query in quizzes page, work on quiz

--- lib/views/quizzes_games_page.dart
+++ lib/views/quizzes_games_page.dart
@@ -10,6 +10,22 @@ class QuizzesGamesPage extends StatefulWidget {
 class _QuizzesGamesPageState extends State<QuizzesGamesPage> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView();
+    return SingleChildScrollView(
+      child: Container(
+        margin: EdgeInsets.only(top: 300),
+        child: Column(
+          children: [
+            Text(
+              'Quizzes and Games',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(),
+          )
+          ],
+        ),
+      ),
+    );
   }
 }
