bd642b1490f98bfdfd1b5f467bb2d8ff78bdca04
nadinnah <nadinahmedmohamed@gmail.com>
2025-11-16 23:44:07 +0200

    add routes in main

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:puffpal/views/common/app_shell.dart';
 import 'package:puffpal/views/home_page.dart';
 import 'package:puffpal/views/login_page.dart';
 import 'package:puffpal/views/profile_page.dart';
+import 'package:puffpal/views/quizzes_games_page.dart';
 import 'package:puffpal/views/signup_page.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -63,6 +64,9 @@ class App extends StatelessWidget {
             routes: {
               '/login': (context) => LoginPage(),
               '/signup': (context) => SignupPage(),
+              '/home': (context) => HomePage(),
+              '/profile': (context) => ProfilePage(),
+              '/quizzes': (context) => QuizzesGamesPage(),
               '/appshell': (context) => AppShell(),
               '/onboarding': (context) => OnboardingScreen(),
             },
