4ac37c03e995bb98070effe6db2f1bbae21b8290
nadinnah <nadinahmedmohamed@gmail.com>
2026-04-19 18:08:48 +0300

    change column to ListView.builder in quizzes_games_page.dart

--- lib/models/quiz.dart
+++ lib/models/quiz.dart
@@ -90,4 +90,79 @@ List<Quiz> quizzes = [
       ),
     ],
   ),
+  Quiz(
+    title: 'title1',
+    image: 'assets/images/inhaler3.png',
+    questions: [
+      Question(
+        "What is asthma?",
+        [
+          "A chronic disease that affects the airways",
+          "A type of heart disease",
+          "A skin condition",
+          "A digestive disorder"
+        ],
+      ),
+      Question(
+        "Which of the following is a common symptom of asthma?",
+        [
+          "Shortness of breath",
+          "Hair loss",
+          "Blurred vision",
+          "Tooth pain"
+        ],
+      ),
+    ],
+  ),
+  Quiz(
+    title: 'title2',
+    image: 'assets/images/trialAsthma.png',
+    questions: [
+      Question(
+        "What can trigger an asthma attack?",
+        [
+          "Dust and pollen",
+          "Watching TV",
+          "Drinking water",
+          "Sleeping"
+        ],
+      ),
+      Question(
+        "Which medication is commonly used for quick relief during an asthma attack?",
+        [
+          "Inhaler (bronchodilator)",
+          "Antibiotics",
+          "Painkillers",
+          "Vitamins"
+        ],
+      ),
+      Question(
+        "How can asthma be managed?",
+        [
+          "Using prescribed medication and avoiding triggers",
+          "Ignoring symptoms",
+          "Only drinking water",
+          "Sleeping more"
+        ],
+      ),
+      Question(
+        "Which of the following is NOT a symptom of asthma?",
+        [
+          "Chest tightness",
+          "Wheezing",
+          "Frequent sneezing",
+          "Shortness of breath"
+        ],
+      ),
+      Question(
+        "What happens to the airways during an asthma attack?",
+        [
+          "They become inflamed and narrow",
+          "They expand permanently",
+          "They stop functioning completely",
+          "Nothing happens"
+        ],
+      ),
+    ],
+  ),
 ];
--- lib/views/quizzes_games_page.dart
+++ lib/views/quizzes_games_page.dart
@@ -35,219 +35,194 @@ class _QuizzesGamesPageState extends State<QuizzesGamesPage> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(30.0, 120, 30, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => PdfViewerPage()),
-                );
-              },
-              child: Container(
-                width: double.infinity,
-                padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topRight,
-                    end: Alignment.bottomLeft,
-                    colors: [
-                      Color(0xF54D97D5),
-                      Color(0xFF1E6096),
-                    ],
-                  ),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Row(
-                  children: [
-                    Icon(Icons.picture_as_pdf, color: Colors.white, size: 32),
-                    SizedBox(width: 20),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Asthma Action Plan",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          "Open PDF",
-                          style: TextStyle(color: Colors.white70, fontSize: 14),
-                        ),
-                      ],
-                    ),
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(30.0, 30, 30, 0),
+      child: ListView(
+        children: [
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => PdfViewerPage()),
+              );
+            },
+            child: Container(
+              width: double.infinity,
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Color(0xF54D97D5),
+                    Color(0xFF1E6096),
                   ],
                 ),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Row(
+                children: [
+                  Icon(Icons.picture_as_pdf, color: Colors.white, size: 32),
+                  SizedBox(width: 20),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Asthma Action Plan",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        "Open PDF",
+                        style: TextStyle(color: Colors.white70, fontSize: 14),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
-            SizedBox(height: 30),
-            const Divider(),
-            SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  AppLocalizations.of(context)!.quizzesTitle,
-                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 30),
+          const Divider(),
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                AppLocalizations.of(context)!.quizzesTitle,
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 5, top: 5),
+                child: IconButton(
+                  icon: Icon(Icons.refresh),
+                  color: Colors.black,
+                  iconSize: 30,
+                  onPressed: () async {
+                    await QuizProgressService.reset();
+                    setState(() {
+                      quizScores.clear(); // remove ticks instantly
+                    });
+                  },
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 5, top: 5),
-                  child: IconButton(
-                    icon: Icon(Icons.refresh),
-                    color: Colors.black,
-                    iconSize: 30,
-                    onPressed: () async {
-                      await QuizProgressService.reset();
-                      setState(() {
-                        quizScores.clear(); // remove ticks instantly
-                      });
-                    },
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 10),
-            Column(
-              children: List.generate(quizzes.length, (index) {
-                final quiz = quizzes[index];
-                final bool isPlayed = quizScores[quiz.title] != null;
-                final int? score = quizScores[quiz.title];
-                return Opacity(
-                  opacity: isPlayed ? 0.7 : 1,
-                  child: Hero(
-                    tag: quizzes[index].image,
-                    child: GestureDetector(
-                      onTap: isPlayed
-                          ? null
-                          : () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) =>
-                                      QuestionsPage(quiz: quizzes[index]),
-                                ),
-                              ).then((_) {
-                                loadProgress(); // refresh the ticks
-                              });
-                            },
-                      child: Stack(
-                        children: [
-                          Container(
-                            margin: EdgeInsets.only(top: index == 0 ? 40 : 60),
-                            child: Material(
+              ),
+            ],
+          ),
+          ...quizzes.map((quiz) {
+            final bool isPlayed = quizScores[quiz.title] != null;
+            final int? score = quizScores[quiz.title];
+
+            return Opacity(
+              opacity: isPlayed ? 0.7 : 1,
+              child: Hero(
+                tag: quiz.image,
+                child: GestureDetector(
+                  onTap: isPlayed
+                      ? null
+                      : () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => QuestionsPage(quiz: quiz),
+                      ),
+                    ).then((_) {
+                      loadProgress();
+                    });
+                  },
+                  child: Stack(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 40),
+                        child: Material(
+                          borderRadius: BorderRadius.circular(20),
+                          elevation: 5,
+                          child: Container(
+                            width: double.infinity,
+                            padding: EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                begin: Alignment.topRight,
+                                end: Alignment.bottomLeft,
+                                colors: [
+                                  Color(0xF54D97D5),
+                                  Color(0xFF1E6096),
+                                ],
+                              ),
                               borderRadius: BorderRadius.circular(20),
-                              elevation: 5,
-                              child: Container(
-                                width: double.infinity,
-                                padding: EdgeInsets.all(20),
-                                decoration: BoxDecoration(
-                                  gradient: LinearGradient(
-                                    begin: Alignment.topRight,
-                                    end: Alignment.bottomLeft,
-                                    colors: [
-                                      Color(0xF54D97D5),
-                                      Color(0xFF1E6096),
-                                    ],
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  padding: EdgeInsets.all(5),
+                                  decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.white70),
+                                    borderRadius: BorderRadius.circular(10),
                                   ),
-                                  borderRadius: BorderRadius.circular(20),
+                                  child: Icon(
+                                    isPlayed ? Icons.done : Icons.play_arrow,
+                                    color: Colors.white,
+                                    size: 30,
+                                  ),
                                 ),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                SizedBox(height: 10),
+                                Text(
+                                  quiz.title,
+                                  style: TextStyle(
+                                    color: Color(0xE0FBFAFA),
+                                    fontSize: 28,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Row(
                                   children: [
-                                    Container(
-                                      padding: EdgeInsets.all(5),
-                                      decoration: BoxDecoration(
-                                        border: Border.all(
-                                          color: Colors.white70,
-                                        ),
-                                        borderRadius: BorderRadius.circular(10),
-                                      ),
-                                      child: isPlayed
-                                          ? Icon(
-                                              Icons.done,
-                                              color: Colors.white,
-                                              size: 30,
-                                            )
-                                          : Icon(
-                                              Icons.play_arrow,
-                                              color: Colors.white,
-                                              size: 30,
-                                            ),
-                                    ),
-                                    SizedBox(height: 10),
                                     Text(
-                                      quizzes[index].title,
-                                      style: TextStyle(
-                                        color: Color(0xE0FBFAFA),
-                                        fontSize: 28,
+                                      quiz.noOfQuestions,
+                                      style: const TextStyle(
+                                        color: Colors.white60,
+                                        fontSize: 14,
                                         fontWeight: FontWeight.bold,
                                       ),
                                     ),
-                                    Row(
-                                      children: [
-                                        Text(
-                                          quiz.noOfQuestions,
-                                          style: const TextStyle(
-                                            color: Colors.white60,
-                                            fontSize: 14,
-                                            fontWeight: FontWeight.bold,
-                                          ),
+                                    if (isPlayed) ...[
+                                      const Text(" • ",
+                                          style: TextStyle(color: Colors.white60)),
+                                      Text(
+                                        "Score: $score/${quiz.questions.length}",
+                                        style: const TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.bold,
                                         ),
-                                        if (isPlayed) ...[
-                                          const Text(
-                                            " • ",
-                                            style: TextStyle(
-                                              color: Colors.white60,
-                                            ),
-                                          ),
-                                          Text(
-                                            "Score: $score/${quiz.questions.length}",
-                                            style: const TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 14,
-                                              fontWeight: FontWeight.bold,
-                                            ),
-                                          ),
-                                        ],
-                                      ],
-                                    ),
+                                      ),
+                                    ],
                                   ],
-                                ),
-                              ),
-                            ),
-                          ),
-                          PositionedDirectional(
-                            end: 0,
-                            top: index == 0 ? -30 : 0,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                Image.asset(
-                                  quizzes[index].image,
-                                  height: 150,
-                                  width: 150,
                                 ),
                               ],
                             ),
                           ),
-                        ],
+                        ),
                       ),
-                    ),
+                      PositionedDirectional(
+                        end: 0,
+                        top: -5,
+                        child: Image.asset(
+                          quiz.image,
+                          height: 150,
+                          width: 150,
+                        ),
+                      ),
+                    ],
                   ),
-                );
-              }),
-            ),
-
-            SizedBox(height: 80),
-          ],
-        ),
+                ),
+              ),
+            );
+          }).toList(),
+          SizedBox(height: 20),
+        ],
       ),
     );
   }
