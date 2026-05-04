31898e6e291e7be517039676c130b129372ff05c
nadinnah <nadinahmedmohamed@gmail.com>
2026-05-04 13:20:32 +0300

    lock device orientation

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,9 @@ import 'firebase_options.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:puffpal/l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/services.dart';
 
-
+var kColorScheme = ColorScheme.fromSeed(seedColor: Color(0xFF1E6097));
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,30 +29,36 @@ Future<void> main() async {
 
   await dotenv.load(fileName: ".env");
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   FirebaseAnalytics.instance;
 
   SharedPreferences prefs = await SharedPreferences.getInstance();
-  final initScreen = prefs.getBool("initScreen")?? false;
-  final notificationsEnabled= prefs.getBool("notificationsEnabled")?? false;
+  final initScreen = prefs.getBool("initScreen") ?? false;
+  final notificationsEnabled = prefs.getBool("notificationsEnabled") ?? false;
 
   await FirebaseApi().initNotification();
   final notificationService = LocalNotificationService(); // Use the singleton
   await notificationService.init();
   await notificationService.requestPermissions();
 
-
-  runApp(MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => LanguageController()),
-        ChangeNotifierProvider(create: (_) => UserProvider()),
-      ], child: App(initScreen: initScreen)));
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((
+    fn,
+  ) {
+    runApp(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (_) => LanguageController()),
+          ChangeNotifierProvider(create: (_) => UserProvider()),
+        ],
+        child: App(initScreen: initScreen),
+      ),
+    );
+  });
 }
 
 class App extends StatelessWidget {
   final bool initScreen;
+
   const App({super.key, required this.initScreen});
 
   @override
@@ -60,34 +67,30 @@ class App extends StatelessWidget {
     final double screenHeight = MediaQuery.of(context).size.height;
 
     final languageController = context.watch<LanguageController>();
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'PuffPal',
-          locale: languageController.locale,
-          supportedLocales: const [
-            Locale('en'),
-            Locale('ar'),
-          ],
-          localizationsDelegates: const [
-            AppLocalizations.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-          ],
-          routes: {
-            '/login': (context) => LoginPage(),
-            '/signup': (context) => SignupPage(),
-            '/home': (context) => HomePage(),
-            '/profile': (context) => ProfilePage(),
-            '/quizzes': (context) => QuizzesGamesPage(),
-            '/appshell': (context) => AppShell(),
-            '/onboarding': (context) => OnboardingScreen(),
-          },
-            //home: initScreen ? LoginPage() : OnboardingScreen());
-            home: initScreen ? AppShell() : OnboardingScreen());
-            //home: OnboardingScreen());
-            //home: initScreen ? const AppShell() : const OnboardingScreen(),
-
-
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'PuffPal',
+      locale: languageController.locale,
+      supportedLocales: const [Locale('en'), Locale('ar')],
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      routes: {
+        '/login': (context) => LoginPage(),
+        '/signup': (context) => SignupPage(),
+        '/home': (context) => HomePage(),
+        '/profile': (context) => ProfilePage(),
+        '/quizzes': (context) => QuizzesGamesPage(),
+        '/appshell': (context) => AppShell(),
+        '/onboarding': (context) => OnboardingScreen(),
+      },
+      //home: initScreen ? LoginPage() : OnboardingScreen());
+      home: initScreen ? AppShell() : OnboardingScreen(),
+    );
+    //home: OnboardingScreen());
+    //home: initScreen ? const AppShell() : const OnboardingScreen(),
   }
 }
